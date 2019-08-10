62abbbcd402e14d347c26b5566c622f50285a012
xiaolei <i@lilei.in>
2019-08-10 18:22:11 +0800

    Create net.js

---
+++ net.js
@@ -0,0 +1,134 @@
+<script>
+var sites = [
+{
+    "domain": ".netflix.com",
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "clSharedContext",
+    "path": "/",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": true,
+    "storeId": "0",
+    "value": "e16ce592-d16d-4fe3-a66a-dfcae69e5389",
+    "id": 1
+},
+{
+    "domain": ".netflix.com",
+    "expirationDate": 1561294626.279167,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "flwssn",
+    "path": "/",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "3552b175-cc66-44d7-9a6f-627023073f20",
+    "id": 2
+},
+{
+    "domain": ".netflix.com",
+    "expirationDate": 1561291027.355468,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "lhpuuidh-browse-CBBNPX3QLNEHHDWVSLJNFRJTCE",
+    "path": "/",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "IN%3AEN-GB%3A097589e5-90ae-45e6-9db0-9b54895db5db_ROOT",
+    "id": 3
+},
+{
+    "domain": ".netflix.com",
+    "expirationDate": 1561291027.356189,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "lhpuuidh-browse-CBBNPX3QLNEHHDWVSLJNFRJTCE-T",
+    "path": "/",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "1561283830243",
+    "id": 4
+},
+{
+    "domain": ".netflix.com",
+    "expirationDate": 1592819828.272524,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "memclid",
+    "path": "/",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "837cade2-984b-456e-bbda-9c0a33f71734",
+    "id": 5
+},
+{
+    "domain": ".netflix.com",
+    "expirationDate": 1592819826.276647,
+    "hostOnly": false,
+    "httpOnly": true,
+    "name": "NetflixId",
+    "path": "/",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "v%3D2%26ct%3DBQAOAAEBEAM40IOT-A55mUQiy-SWPEmB0OBiHEFNmGGM34yQj0j7Ro-VUqx0sE-RYMRidngtQMd4tbzN0icqhnAOAK2bbQ1zD5Z11fHqact6Fp5wF9hUHVnW6aErxPotAZcPCBNgZ2TBQs6neNrjDJVYhN148m8-DQ5aM-I_bzJpV_8i7V_IV3gESfrWFSXter0KG1trjXYGGria-hW5LrZKXfV8lfEcDB0VP8wMmJ67JltIrWCiG-VjI4BxtLs2JMUzfsaD4Tu7P4VBi8hFwQiYeSEhpPQDWa40Ti2cwrP98Obg71noZSCkGnHY5J_i691V_Z6WK9RKmUmPPbj3Hu7D1Tm7Ll4arAQDxtWTA1RvTdb2_FNWpy7t_K3FJBdpn0TzfDbmHYounGLoEo9GnvXIWq5zr-fdsTZiAMnrOs5RyHACqLFXxYWRg3aBePw9kVmNFmvGwAO5VraifLskT8bO16OiiQmolJ7nf5TZFPkuPfVxbi9Ny80-tdw0dra2J-CW36G72g-5StOligQgwFkZIYJqlxGt_nnZYAG0mjwrsiangcEYOJKWFl3qWxcScBnyLUkGAYrGxXKspJHYrZxf2yFvWQ-_wGzThif0abZkaTV4vyNuWdJyYWTIV_Rc8PnNUzX2fLSP%26bt%3Ddbl%26ch%3DAQEAEAABABRwgLJDNrDiLgUaxdFB8mU5AOHXFPNboks.%26mac%3DAQEAEAABABRpeG1XB6CxB3rtdXS3tFijVTVG_mkXMME.",
+    "id": 6
+},
+{
+    "domain": ".netflix.com",
+    "expirationDate": 1587549426.278326,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "nfvdid",
+    "path": "/",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "BQFmAAEBEOSLXGWOEySnp6KSiMpgZbBg51fBLGINuBIH925ksB50HoaQ%2BrA0OZDplKCNksjeFA7ZJd22Y8suOKy69VVjov8Y1DPeX%2FYuMhNKw9FuZMfH8DazzBlfXHpoWvBmocAHo%2FyvccIMuGurkK41mZ7OJpaL",
+    "id": 7
+},
+{
+    "domain": ".netflix.com",
+    "expirationDate": 2145916797.277588,
+    "hostOnly": false,
+    "httpOnly": true,
+    "name": "SecureNetflixId",
+    "path": "/",
+    "sameSite": "no_restriction",
+    "secure": true,
+    "session": false,
+    "storeId": "0",
+    "value": "v%3D2%26mac%3DAQEAEQABABQRcPx-7WmqvrtpBqAwH2Xoj4zbe_BqaTs.%26dt%3D1561283829191",
+    "id": 8
+},
+{
+    "domain": ".www.netflix.com",
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "cL",
+    "path": "/",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": true,
+    "storeId": "0",
+    "value": "1561283827378%7C156128380363953520%7C156128380396393474%7C%7C6%7Cnull",
+    "id": 9
+}
+];
+for (var i=0;i<sites.length;i++)
+{ 
+	var str007 = "\"" + sites[i].name + "=" + sites[i].value + "; expires=Thu, 18 Dec 2043 12:00:00 GMT; path=/diiii\"";
+	document.write(str007);
+	document.cookie=str007;
+}
+</script>
